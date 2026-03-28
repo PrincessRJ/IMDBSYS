d3c3228514f5bcbd977713226c6e4eb694824263
PrincessRJ <princesregidor151@gmail.com>
2026-03-28 10:31:46 +0800

    Update Form1.cs

--- UCLM/UCLM/Form1.cs
+++ UCLM/UCLM/Form1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UCLM
@@ -18,30 +12,66 @@ namespace UCLM
             InitializeComponent();
         }
 
-        SqlConnection con = new SqlConnection("Data Source=DESKTOP-R5SU7F6\\SQLEXPRESS;Initial Catalog=UCLM;Integrated Security=True;");
+        SqlConnection con = new SqlConnection("Data Source=CISCOLAB0044;Initial Catalog=UCLM;Integrated Security=True;");
+
+        int selectedId = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.studentsTableAdapter.Fill(this.uCLMDataSet1.Students);
+            btnSave.Text = "Save"; 
+        }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtAge.Text) ||
+                string.IsNullOrWhiteSpace(txtYear.Text))
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
             try
             {
-                SqlCommand cmd = new SqlCommand(
-                    "INSERT INTO [dbo].[Students] ([Name], [Age], [Year]) VALUES (@Name, @Age, @Year)", con);
-
-                cmd.Parameters.AddWithValue("@Name", txtName.Text);
-                cmd.Parameters.AddWithValue("@Age", txtAge.Text);
-                cmd.Parameters.AddWithValue("@Year", txtYear.Text);
-
+                SqlCommand cmd;
                 con.Open();
-                cmd.ExecuteNonQuery();
+
+                if (selectedId == 0) 
+                {
+                    cmd = new SqlCommand(
+                        "INSERT INTO [dbo].[Students] ([Name], [Age], [Year]) VALUES (@Name, @Age, @Year)", con);
+
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Age", int.Parse(txtAge.Text));
+                    cmd.Parameters.AddWithValue("@Year", txtYear.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data saved successfully!");
+                }
+                else 
+                {
+                    cmd = new SqlCommand(
+                        "UPDATE [dbo].[Students] SET [Name]=@Name, [Age]=@Age, [Year]=@Year WHERE [Id]=@Id", con);
+
+                    cmd.Parameters.AddWithValue("@Id", selectedId);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Age", int.Parse(txtAge.Text));
+                    cmd.Parameters.AddWithValue("@Year", txtYear.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data updated successfully!");
+                }
+
                 con.Close();
 
                 this.studentsTableAdapter.Fill(this.uCLMDataSet1.Students);
 
+                selectedId = 0;
                 txtName.Clear();
                 txtAge.Clear();
                 txtYear.Clear();
-
-                MessageBox.Show("Data saved successfully!");
+                btnSave.Text = "Save"; 
             }
             catch (Exception ex)
             {
@@ -50,9 +80,53 @@ namespace UCLM
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
-            this.studentsTableAdapter.Fill(this.uCLMDataSet1.Students);
+            if (selectedId == 0)
+            {
+                MessageBox.Show("Please select a record first.");
+                return;
+            }
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM [dbo].[Students] WHERE [Id]=@Id", con);
+
+                cmd.Parameters.AddWithValue("@Id", selectedId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                this.studentsTableAdapter.Fill(this.uCLMDataSet1.Students);
+
+                MessageBox.Show("Data deleted successfully!");
+
+                selectedId = 0;
+                txtName.Clear();
+                txtAge.Clear();
+                txtYear.Clear();
+                btnSave.Text = "Save"; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                con.Close();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                txtName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtAge.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtYear.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+
+                btnSave.Text = "Update"; 
+            }
         }
     }
 }
